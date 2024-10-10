88ca7806c2701e1a7068c5dc4d53e9fe152dd97b
rodwanz <wanzervai@gmail.com>
2024-10-10 18:02:04 -0300

    Criando rota Post, e usando Insomnia para requisição

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const app = express()
+app.use(express.json())
 
 const selecoes = [
     {id:1, selecao: 'Brasil', grupo: "G"},
@@ -15,6 +16,11 @@ app.get('/', (req, res) => {
 
 app.get('/selecoes', (req, res) =>{
     res.status(200).send (selecoes)
+})
+
+app.post('/selecoes', (req, res) =>{
+    selecoes.push(req.body)
+    res.status(201).send ("Seleção cadastrada com sucesso!")
 })
 
 export default app
