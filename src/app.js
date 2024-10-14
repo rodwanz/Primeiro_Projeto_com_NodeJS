98f7c81d6314f672ae26172f1bb8df62bb27c6a7
rodwanz <wanzervai@gmail.com>
2024-10-14 11:15:15 -0300

    Busacando seleção por ID e criando rota Delete

--- src/app.js
+++ src/app.js
@@ -9,6 +9,13 @@ const selecoes = [
     {id:4, selecao: 'Camarões', grupo: "G"},
 ]
 
+function buscarSelecaoPorId(id){
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
 
 app.get('/', (req, res) => {
     res.send('Iniciando, dando passos largos com funções!')
@@ -18,9 +25,19 @@ app.get('/selecoes', (req, res) =>{
     res.status(200).send (selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) =>{
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) =>{
     selecoes.push(req.body)
     res.status(201).send ("Seleção cadastrada com sucesso!")
+})
+
+app.delete('/selecoes/:id', (req, res) =>{
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso!`)
 })
 
 export default app
